8e4e259bb8d6278d7bdf251642394096d659304d
bastien <bastien@wink-dev.com>
2020-03-18 16:03:16 +0100

    TP 8

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TodoItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,19 +11,32 @@ class TodoController extends AbstractController
     /**
      * @Route("/list", name="todo_list")
      */
-    public function list()
+    public function list(TodoItemRepository $todoRepo)
     {
-        $todoListExample = [
-            'aller sur la lune',
-            'acheter du pain',
-            'donner à une association'
-        ];
+        //$todoListExample = [
+        //    'aller sur la lune',
+        //    'acheter du pain',
+        //    'donner à une association'
+        //];
+
+        // TP 8.1
+        //$todoList = $todoRepo->findAll();
+
+        // TP 8.2
+        //$todoList = $todoRepo->findByDone(false);
+        //$todoList = $todoRepo->findBy(array('done' => false]);
+        $todoList = $todoRepo->findBy(['done' => false]);
+
+        $countResult = $todoRepo->count(['done' => false]);
 
         // TodoList sera le nom de la variable dans mon fichier twig
         // Elle contient le tableau de Todo
         return $this->render(
             'todo/list.html.twig',
-            ["todoList" => $todoListExample]
+            [
+                "todoList" => $todoList,
+                "countResult" => $countResult
+            ]
         );
     }
 
@@ -30,11 +44,13 @@ class TodoController extends AbstractController
     /**
      * @Route("/detail/{id}", name="todo_detail")
      */
-    public function detail($id = 1)
+    public function detail($id = 1, TodoItemRepository $todoRepo)
     {
+        $todoItem = $todoRepo->find($id);
+
         return $this->render(
             'todo/detail.html.twig',
-            ["idTodo" => $id]
+            ["todoItem" => $todoItem]
         );
     }
 
